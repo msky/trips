504a6ea8a3c4fb2bffbd367f0492e757809ddbfe
msky <maciejewskym@gmail.com>
2019-12-29 13:13:53 +0100

    init spring boot project

---
+++ src/test/java/msky/trips/TripsApplicationTests.java
@@ -0,0 +1,13 @@
+package msky.trips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
